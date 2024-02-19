8854984f649557c0d0a41b6be82c070c1d36964d
Nasir <Nasirjamaelmi@outlook.com>
2024-02-19 15:03:04 +0100

    Champions Leauge API
    Loading the footballmodel with data from the api to use for the champions leauge section.

--- Model/championsgetdata.swift
+++ Model/championsgetdata.swift
@@ -6,4 +6,40 @@
 //
 
 import Foundation
+import Observation
 
+
+@Observable
+class FootballModel{
+    
+    var footballData: ChampionsLeagueData?
+    
+    var isLoading = false
+    
+    
+    func loadfeed() async {
+        guard let url = URL(string:
+                                "https://www.fotmob.com/_next/data/4355/en/leagues/42/overview/champions-league.json?id=42&tab=overview&slug=champions-league"
+                            
+        )
+        else {
+            return
+        }
+        isLoading = true
+        
+        
+        do{
+            let (data,_) = try await URLSession.shared.data(from: url)
+            print(data)
+            let decodeData = try JSONDecoder().decode(ChampionsLeagueData.self, from: data)
+            self.footballData = decodeData
+            
+            
+        }
+        catch{
+            print(error)
+        }
+        isLoading = false
+    }
+}
+    
